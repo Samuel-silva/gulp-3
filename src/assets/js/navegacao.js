13bfc2e2849e0dff2b5c9917fd9a4f4356b9a9d0
Samuel Barbosa <samuca192silva@hotmail.com>
2023-12-03 17:37:29 -0300

    feat - Quill editor

--- src/assets/js/navegacao.js
+++ src/assets/js/navegacao.js
@@ -1,3 +1,33 @@
 (function () {
+  const toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    ['blockquote', 'code-block'],
 
+    [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+    [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+    [{ 'script': 'sub' }, { 'script': 'super' }],      // superscript/subscript
+    [{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indentx
+    [{ 'direction': 'rtl' }],                         // text direction
+
+    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+    [{ 'font': [] }],
+    [{ 'align': [] }]
+  ];
+
+  const quill = new Quill('#quill-editor', {
+    modules: {
+      toolbar: toolbarOptions
+    },
+    theme: 'snow'
+  });
+
+  $( "#salvarQuill" ).on( "click", function() {
+    const contents = quill.getContents();
+    const html = JSON.stringify(contents, null, 2);
+    $('#codeQuill').text(html)
+    // console.log(html)
+  });
 })()
